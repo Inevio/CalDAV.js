0daf65890b4a05dae667ade0bd25bc17982bdb65
Rodrigo Rojo <rrojot@hotmail.com>
2014-12-24 01:13:53 +0100

    Calendar functions and createEvent added
    Revise createEvent function and the new functions' response.

--- sync.js
+++ sync.js
@@ -8,6 +8,119 @@ var Sync = function( config ){
 
 };
 
+Sync.prototype.createCalendar = function ( info, path, callback ) {
+
+	var body = '';
+
+	body += '<?xml version="1.0" encoding="UTF-8"?>\r\n';
+	body += '<B:mkcalendar xmlns:B="urn:ietf:params:xml:ns:caldav">\r\n';
+	body += '<A:set xmlns:A="DAV:">\r\n';
+	body += '<A:prop>\r\n';
+	body += '<D:calendar-color xmlns:D="http://apple.com/ns/ical/" symbolic-color="'+ info.color.name +'">'+ info.color.hex +'</D:calendar-color>\r\n';
+	body += '<A:displayname>'+ info.name +'</A:displayname>\r\n';
+	// MIRAR ESTE ATRIBUTO 
+	body += '<D:calendar-order xmlns:D="http://apple.com/ns/ical/">7</D:calendar-order>\r\n';
+	body += '<B:calendar-free-busy-set>	<YES/> </B:calendar-free-busy-set>\r\n';
+	body += '<B:calendar-timezone>\r\n';
+		//PROPIEDADES DE LA TIMEZONE
+		body += 'BEGIN:VCALENDAR;\r\n';
+		body += 'VERSION:2.0;\r\n';
+		body += 'PRODID:-//Apple Inc.//Mac OS X 10.10.1//EN;\r\n';
+		body += 'CALSCALE:GREGORIAN;\r\n';
+		body += 'BEGIN:VTIMEZONE;\r\n';
+		body += 'TZID:Europe/Madrid;\r\n';
+		body += 'BEGIN:DAYLIGHT;\r\n';
+		body += 'TZOFFSETFROM:+0100;\r\n';
+		body += 'RRULE:FREQ=YEARLY;BYMONTH=3;BYDAY=-1SU;\r\n';
+        body += 'DTSTART:19810329T020000;\r\n';
+        body += 'TZNAME:CEST;\r\n';
+        body += 'TZOFFSETTO:+0200;\r\n';
+        body += 'END:DAYLIGHT;\r\n';
+        body += 'BEGIN:STANDARD;\r\n';
+        body += 'TZOFFSETFROM:+0200;\r\n';
+        body += 'RRULE:FREQ=YEARLY;BYMONTH=10;BYDAY=-1SU;\r\n';
+        body += 'DTSTART:19961027T030000;\r\n';
+        body += 'TZNAME:CET;\r\n';
+        body += 'TZOFFSETTO:+0100;\r\n';
+        body += 'END:STANDARD;\r\n';
+        body += 'END:VTIMEZONE;\r\n';
+        body += 'END:VCALENDAR;\r\n';
+    body += '</B:calendar-timezone>\r\n';
+    body += '<B:supported-calendar-component-set>\r\n';
+    body += '<B:comp name="VEVENT"/>\r\n';
+    body += '</B:supported-calendar-component-set>\r\n';
+    body += '</A:prop>\r\n';
+    body += '</A:set>\r\n';
+	body += '</B:mkcalendar>\r\n';
+
+	console.log( path + '/' + tools.uuid());
+
+	this.request('MKCOL', path + '/' + tools.uuid() + '/', body, function (err, res) {
+		if (err) {
+			callback(err);
+			return;
+		}
+		callback(null, res);
+	});
+
+};
+
+Sync.prototype.createEvent = function (info, calendar, callback ) {
+
+	var body = "";
+
+	body += 'BEGIN:VCALENDAR\r\n';
+	body += 'VERSION:2.0\r\n';
+	body += 'PRODID:-//Apple Inc.//Mac OS X 10.10.1//EN\r\n';
+	body += 'CALSCALE:GREGORIAN\r\n';
+	body += 'BEGIN:VTIMEZONE\r\n';
+	body += 'TZID:Europe/Madrid\r\n';
+	body += 'BEGIN:DAYLIGHT\r\n';
+	body += 'TZOFFSETFROM:+0100\r\n';
+	body += 'RRULE:FREQ=YEARLY;BYMONTH=3;BYDAY=-1SU\r\n';
+	body += 'DTSTART:19810329T020000\r\n';
+	body += 'TZNAME:CEST\r\n';
+	body += 'TZOFFSETTO:+0200\r\n';
+	body += 'END:DAYLIGHT\r\n';
+	body += 'BEGIN:STANDARD\r\n';
+	body += 'TZOFFSETFROM:+0200\r\n';
+	body += 'RRULE:FREQ=YEARLY;BYMONTH=10;BYDAY=-1SU\r\n';
+	body += 'DTSTART:19961027T030000\r\n';
+	body += 'TZNAME:CET\r\n';
+	body += 'TZOFFSETTO:+0100\r\n';
+	body += 'END:STANDARD\r\n';
+	body += 'END:VTIMEZONE\r\n';
+	body += 'BEGIN:VEVENT\r\n';
+	body += 'CREATED:20141223T193820Z\r\n';
+	body += 'UID:F5EC8666-662C-47F2-83E4-9F2269CC31AF\r\n';
+	body += 'DTEND;TZID=Europe/Madrid:20141223T220000\r\n';
+	body += 'TRANSP:OPAQUE\r\n';
+	body += 'SUMMARY:' + info.name + '\r\n';
+	body += 'DTSTART;TZID=Europe/Madrid:20141223T210000\r\n';
+	body += 'DTSTAMP:20141223T193820Z\r\n';
+	body += 'SEQUENCE:0\r\n';
+	body += 'END:VEVENT\r\n';
+	body += 'END:VCALENDAR\r\n';
+
+	this.request( 'PUT', calendar + tools.uuid() + '.ics', body, function (err, res) {
+		if (err) {
+			cosole.log(err);
+			return;
+		}
+
+		callback(null);
+	});
+
+};
+
+Sync.prototype.deleteCalendar = function ( calendar, callback ) {
+
+	this.request('DELETE', calendar, '', function (err, res) {
+		callback(err);
+	});
+
+};
+
 Sync.prototype.getCalendars = function( calHome, callback ){
 
 	var body = '';
@@ -204,57 +317,13 @@ Sync.prototype.getCalendarHome = function( home, callback ){
 
 };
 
-Sync.prototype.createCalendar = function ( info, callback ) {
+Sync.prototype.renameCalendar = function ( name, calendar, callback ) {
 
-	var body = '';
+	var body = '<?xml version="1.0" encoding="UTF-8"?>\r\n';
+	body += '<A:propertyupdate xmlns:A="DAV:"><A:set><A:prop><A:displayname>'+ name +'</A:displayname></A:prop></A:set></A:propertyupdate>';
 
-	body += '<?xml version="1.0" encoding="UTF-8"?>';
-	body += '<B:mkcalendar xmlns:B="urn:ietf:params:xml:ns:caldav">';
-	body += '<A:set xmlns:A="DAV:">';
-	body += '<A:prop>';
-	body += '<D:calendar-color xmlns:D="http://apple.com/ns/ical/" symbolic-color="'+ info.color.name +'">'+ info.color.hex +'</D:calendar-color>'
-	body += '<A:displayname>'+ info.name +'</A:displayname>';
-	// MIRAR ESTE ATRIBUTO 
-	body += '<D:calendar-order xmlns:D="http://apple.com/ns/ical/">7</D:calendar-order>';
-	body += '<B:calendar-free-busy-set>	<YES/> </B:calendar-free-busy-set>';
-	body += '<B:calendar-timezone>';
-		//PROPIEDADES DE LA TIMEZONE
-		body += 'BEGIN:VCALENDAR&#13;';
-		body += 'VERSION:2.0&#13;';
-		body += 'PRODID:-//Apple Inc.//Mac OS X 10.10.1//EN&#13;';
-		body += 'CALSCALE:GREGORIAN&#13;';
-		body += 'BEGIN:VTIMEZONE&#13;';
-		body += 'TZID:Europe/Madrid&#13;';
-		body += 'BEGIN:DAYLIGHT&#13;';
-		body += 'TZOFFSETFROM:+0100&#13;';
-		body += 'RRULE:FREQ=YEARLY;BYMONTH=3;BYDAY=-1SU&#13;';
-        body += 'DTSTART:19810329T020000&#13;';
-        body += 'TZNAME:CEST&#13;';
-        body += 'TZOFFSETTO:+0200&#13;';
-        body += 'END:DAYLIGHT&#13;';
-        body += 'BEGIN:STANDARD&#13;';
-        body += 'TZOFFSETFROM:+0200&#13;';
-        body += 'RRULE:FREQ=YEARLY;BYMONTH=10;BYDAY=-1SU&#13;';
-        body += 'DTSTART:19961027T030000&#13;';
-        body += 'TZNAME:CET&#13;';
-        body += 'TZOFFSETTO:+0100&#13;';
-        body += 'END:STANDARD&#13;';
-        body += 'END:VTIMEZONE&#13;';
-        body += 'END:VCALENDAR&#13;';
-    body += '</B:calendar-timezone>';
-    body += '<B:supported-calendar-component-set>';
-    body += '<B:comp name="VEVENT"/>';
-    body += '</B:supported-calendar-component-set>';
-    body += '</A:prop>';
-    body += '</A:set>'
-	body += '</B:mkcalendar>';
-
-	this.request('MKCOL', info.path + tools.uuid(), body, function (err, res) {
-		if (err) {
-			callback(err);
-			return;
-		}
-		callback(null, res);
+	this.request( 'PROPPATCH', calendar, body, function (err, res) {
+		console.log(err);
 	});
 
 };
